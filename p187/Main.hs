c35b73fbc79b59a2b4d4389c79ce9cc3ae7d1317
Jakob Runge <sicarius@g4t3.de>
2016-03-26 17:33:28 +0100

    Work on p187

--- p187/Main.hs
+++ p187/Main.hs
@@ -1,44 +1,38 @@
 module Main where
 
-import Control.Monad
+import Data.Set (Set)
+import qualified Data.Set as Set
 
-magic :: Int
-magic = 10^7
+type N = Int
 
-range :: [Int]
+magic :: N
+magic = 10^8
+
+range :: [N]
 range = [4..magic] -- | 4 is the first semiprime
 
-root :: Int -> Int
+primes :: [N]
+primes = 2 : 3 : sieve [] (tail primes) 3
+  where
+  notDivsBy d n     = n `rem` d /= 0
+  sieve ds (p:ps) x = foldr (filter . notDivsBy) [x+2, x+4..p*p-2] ds
+                   ++ sieve (p:ds) ps (p*p)
+
+root :: N -> N
 root = round . sqrt . fromIntegral
 
-primesTill :: Int -> [Int]
-primesTill x
-	| x < 30 = takeWhile (<=x) [2,3,5,7,11,13,17,19,23,29]
-	| otherwise = do
-		let r = root x
-		let primes = primesTill r
-		let newPrimes = do
-			test <- [r..x]
-			guard . all (/=0) $ map (mod test) primes
-			return test
-		primes++newPrimes
+isPrime :: N -> Bool
+isPrime x
+  | x <= 1 = False
+  | otherwise = all (/=0) . map (mod x) $ takeWhile (<=(root x)) primes
 
-primes :: [Int]
-primes = primesTill magic
+subSet :: Set N -> N -> Set N
+subSet s p = fst $ Set.split (magic `div` p) s
 
-primesTillRoot :: [Int]
-primesTillRoot = primesTill $ root magic
-
-isPrime :: Int -> Bool
-isPrime x = all (/=0) . map (mod x) $ takeWhile (<=(root x)) primes
-
-semiprimes :: [Int]
-semiprimes = do
-	sp <- range
-	t <- takeWhile (< root sp) primesTillRoot
-	guard $ t < sp
-	guard $ sp `mod` t == 0
-	guard $ isPrime (sp`div`t)
-	return sp
-
-main = print $ length semiprimes
+foo = let startSet = Set.fromAscList $ takeWhile (<= (magic `div` 2)) primes
+          ps = takeWhile (<= (root magic) primes
+      in go ps startSet
+      where
+        go :: [N] -> Set N -> [Set N]
+        go (p:ps) s = s : go ps $ subS
+        go [] s = [s]
