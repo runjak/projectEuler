76d7088035b46cc387cd8b9a37bc0ac29b8b3cbb
Jakob Runge <sicarius@g4t3.de>
2016-03-31 20:15:51 +0200

    Work on p44

--- p44/Main.hs
+++ p44/Main.hs
@@ -1,1 +1,43 @@
 module Main where
+
+import Control.Monad
+import qualified Data.Maybe as Maybe
+
+type N = Int
+
+pentagonal :: N -> N
+pentagonal n = n*(3*n - 1) `div` 2
+
+unPentagonal :: N -> Maybe N
+unPentagonal n = let y = 2 * fromIntegral n
+                     x = (sqrt (12*y + 1) + 1)/6
+                 in if ceiling x == floor x
+                    then Just $ floor x
+                    else Nothing
+
+isPentagonal :: N -> Bool
+isPentagonal = Maybe.isJust . unPentagonal
+
+pentagonals :: [N]
+pentagonals = map pentagonal [1..]
+
+pPairs :: [(N, N)]
+pPairs = do
+  (n, y) <- zip [1..] pentagonals
+  let next  = pentagonal $ n + 1
+      upper = takeWhile (< y)
+      lower = dropWhile (>= (next - y))
+  x <- upper . lower $ pentagonals
+  return (x, y)
+
+diffs = do
+  (x,y) <- pPairs
+  guard $ isPentagonal $ x + y
+  let d = y - x
+  guard $ isPentagonal d
+  return (d,x,y)
+
+-- This approach is too slow at printing anything useful.
+-- I shall rethink this and figure out something more elegant.
+
+main = print diffs
