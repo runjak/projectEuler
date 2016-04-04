e8e7cf120412516e3cf2311dcf7b55888a92e329
Jakob Runge <sicarius@g4t3.de>
2016-04-04 14:15:21 +0200

    Solve p46

--- done/p46/Main.hs
+++ done/p46/Main.hs
@@ -11,14 +11,6 @@ primes = 2 : 3 : sieve [] (tail primes) 3
   sieve ds (p:ps) x = foldr (filter . notDivsBy) [x+2, x+4..p*p-2] ds
                    ++ sieve (p:ds) ps (p*p)
 
-root :: N -> N
-root = round . sqrt . fromIntegral
-
-isPrime :: N -> Bool
-isPrime x
-  | x <= 1 = False
-  | otherwise = notElem 0 . map (mod x) $ takeWhile (<= root x) primes
-
 composites :: [N]
 composites = go [4..] primes
   where
@@ -31,10 +23,11 @@ squares :: [N]
 squares = [x*x | x <- [1..]]
 
 wanted = do
-  c <- composites
-  p <- takeWhile (< c) primes
-  s <- takeWhile (< (c - p)) squares
-  guard $ c == 2*s + p -- These are the ones we DON'T want!
+  c <- filter odd composites
+  guard $ notElem c $ do
+    p <- takeWhile (< c) primes
+    s <- takeWhile (< (c - p)) squares
+    return $ 2*s+p
   return c
 
 solution = head wanted
