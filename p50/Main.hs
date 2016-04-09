688348c3dce73d177ec146012eff6c775cefd961
Jakob Runge <sicarius@g4t3.de>
2016-04-09 17:07:20 +0200

    Work on p50

--- p50/Main.hs
+++ p50/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
+import Control.Monad
+import Data.Function (on)
 import Data.List
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-type N = Integer
+type N = Int
 
 primes :: [N]
 primes = 2 : 3 : sieve [] (tail primes) 3
@@ -21,23 +23,26 @@ pSet = Set.fromAscList $ takeWhile (<= magic) primes
 isPrime :: N -> Bool
 isPrime = Set.member `flip` pSet
 
--- | This approach filled all my ram and took too long.
---   I should refine it.
+{-
+  I can reduce the searchSpace with the following knowledge:
+     953 is prime and the sum of 21 consecutive primes
+  -> I only need to consider a range, so that n >= 21 primes are <= 10^6.
+  -> I can use this to find an upper limit for the set of primes I need to search.
+  -> I also know that this must be within the first half of pSet,
+     because n >= magic/2 implies that only two terms can be summed
+     up to something bigger than magic.
+-}
 
-sums :: [[N]]
-sums = go ps $ tail ps
+searchSpace' = fst $ Set.split (magic `div` 2) pSet
+
+{- Calculate sequential sums of a list, starting with 0 -}
+seqSums :: [N] -> [N]
+seqSums = seqSums' 0
   where
-    ps = Set.toAscList pSet
+    seqSums' s [] = [s]
+    seqSums' s (n:ns) = s : seqSums' (s+n) ns
 
-    go _ [] = []
-    go [] _ = []
-    go xs ys = let newRow = zipWith (+) xs ys
-               in newRow : go newRow (tail ys)
-
-wanted :: [N]
-wanted = filter isPrime $ concat $ reverse sums
-
-solution :: N
-solution = head wanted
-
-main = print solution
+{- Calculate the sums of all segments of a given length in a given list. -}
+windowSums :: N -> [N] -> [N]
+windowSums width xs = let sums = seqSums xs
+                      in zipWith subtract sums $ drop width sums
