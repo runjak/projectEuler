5ae00ee38401d3b6ccd26f93e00a7c41b447abe9
Jakob Runge <sicarius@g4t3.de>
2016-04-10 22:09:32 +0200

    Work on p90

--- p90/Main.hs
+++ p90/Main.hs
@@ -1,1 +1,15 @@
 module Main where
+
+import Data.Char (digitToInt)
+import Data.List
+
+type N = Int
+
+{-
+  I'm encoding the squares as tuples of digits.
+  From these I want to generate two sets,
+  where each tuple implies, that if one of its digits is in one set,
+  the other digit must be in the other set.
+-}
+squares :: [(N,N)]
+squares = [(0,1),(0,4),(0,9),(1,6),(2,5),(3,6),(4,9),(6,4),(8,1)]
