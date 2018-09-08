033dfb068d43e366a4ae84bc2fb3ebbeea4a5964
Jakob Runge <sicarius@g4t3.de>
2018-09-08 16:16:09 +0200

    simplify Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,69 +1,69 @@
 module Main where
 
 import Control.Monad (forM_)
-import qualified Problem1 as Problem1
-import qualified Problem2 as Problem2
-import qualified Problem3 as Problem3
-import qualified Problem4 as Problem4
-import qualified Problem5 as Problem5
-import qualified Problem6 as Problem6
-import qualified Problem7 as Problem7
-import qualified Problem8 as Problem8
-import qualified Problem9 as Problem9
-import qualified Problem10 as Problem10
-import qualified Problem11 as Problem11
-import qualified Problem12 as Problem12
-import qualified Problem13 as Problem13
-import qualified Problem14 as Problem14
-import qualified Problem15 as Problem15
-import qualified Problem16 as Problem16
-import qualified Problem17 as Problem17
-import qualified Problem18 as Problem18
-import qualified Problem19 as Problem19
-import qualified Problem20 as Problem20
-import qualified Problem21 as Problem21
-import qualified Problem22 as Problem22
-import qualified Problem23 as Problem23
-import qualified Problem24 as Problem24
-import qualified Problem25 as Problem25
-import qualified Problem26 as Problem26
-import qualified Problem27 as Problem27
-import qualified Problem28 as Problem28
-import qualified Problem29 as Problem29
-import qualified Problem30 as Problem30
-import qualified Problem31 as Problem31
-import qualified Problem32 as Problem32
-import qualified Problem33 as Problem33
-import qualified Problem34 as Problem34
-import qualified Problem35 as Problem35
-import qualified Problem36 as Problem36
-import qualified Problem37 as Problem37
-import qualified Problem38 as Problem38
-import qualified Problem39 as Problem39
-import qualified Problem40 as Problem40
-import qualified Problem41 as Problem41
-import qualified Problem42 as Problem42
-import qualified Problem43 as Problem43
-import qualified Problem44 as Problem44
-import qualified Problem45 as Problem45
-import qualified Problem46 as Problem46
-import qualified Problem47 as Problem47
-import qualified Problem48 as Problem48
-import qualified Problem49 as Problem49
-import qualified Problem50 as Problem50
-import qualified Problem52 as Problem52
-import qualified Problem55 as Problem55
-import qualified Problem57 as Problem57
-import qualified Problem58 as Problem58
-import qualified Problem59 as Problem59
-import qualified Problem65 as Problem65
-import qualified Problem79 as Problem79
-import qualified Problem89 as Problem89
-import qualified Problem92 as Problem92
-import qualified Problem96 as Problem96
-import qualified Problem148 as Problem148
-import qualified Problem187 as Problem187
-import qualified Problem215 as Problem215
+import qualified Problem1
+import qualified Problem2
+import qualified Problem3
+import qualified Problem4
+import qualified Problem5
+import qualified Problem6
+import qualified Problem7
+import qualified Problem8
+import qualified Problem9
+import qualified Problem10
+import qualified Problem11
+import qualified Problem12
+import qualified Problem13
+import qualified Problem14
+import qualified Problem15
+import qualified Problem16
+import qualified Problem17
+import qualified Problem18
+import qualified Problem19
+import qualified Problem20
+import qualified Problem21
+import qualified Problem22
+import qualified Problem23
+import qualified Problem24
+import qualified Problem25
+import qualified Problem26
+import qualified Problem27
+import qualified Problem28
+import qualified Problem29
+import qualified Problem30
+import qualified Problem31
+import qualified Problem32
+import qualified Problem33
+import qualified Problem34
+import qualified Problem35
+import qualified Problem36
+import qualified Problem37
+import qualified Problem38
+import qualified Problem39
+import qualified Problem40
+import qualified Problem41
+import qualified Problem42
+import qualified Problem43
+import qualified Problem44
+import qualified Problem45
+import qualified Problem46
+import qualified Problem47
+import qualified Problem48
+import qualified Problem49
+import qualified Problem50
+import qualified Problem52
+import qualified Problem55
+import qualified Problem57
+import qualified Problem58
+import qualified Problem59
+import qualified Problem65
+import qualified Problem79
+import qualified Problem89
+import qualified Problem92
+import qualified Problem96
+import qualified Problem148
+import qualified Problem187
+import qualified Problem215
 
 problems :: [(String, IO ())]
 problems = [("Problem1: ", Problem1.main),
